f73ea15f383f39ce75021b1e43a2706dab340be4
jimmyray92 <jameswang675203@gmail.com>
2022-03-29 22:43:53 +1000

    added feature to allow the user to view current bookings

--- index.rb
+++ index.rb
@@ -1,4 +1,12 @@
 require 'json'
+bookings = [
+    {"@make":"Toyota","@model":"Corolla","@year":"2011","@colour":"White"},
+    {"@make":"Nissan","@model":"Micra","@year":"2012","@colour":"Yellow"},
+
+]
+puts "What would you like to do?"
+user_input = gets.chomp
+
 
 class CarDetails
     attr_accessor :make, :model, :year, :pages
@@ -8,6 +16,22 @@ class CarDetails
     @year = year
     @colour = colour
     end
+
+
+    def add_booking
+        puts "Make: "
+        car_make = gets.chomp
+        puts "Model: "
+        car_model = gets.chomp
+        puts "Year: "
+        car_year = gets.chomp
+        puts "Colour: "
+        car_color = gets.chomp
+        new_car = CarDetails.new(car_make, car_model, car_year, car_color)
+        new_car = new_car.to_json
+        bookings = bookings.push(new_car)
+    end
+
 
     def to_json
         hash = {}
@@ -24,48 +48,51 @@ class CarDetails
     end
 end
 
-
-
-
-
-puts "What would you like to do?"
-user_input = gets.chomp
-car_details = []
-customer_details = []
+    def add_booking
+        puts "Make: "
+        car_make = gets.chomp
+        puts "Model: "
+        car_model = gets.chomp
+        puts "Year: "
+        car_year = gets.chomp
+        puts "Colour: "
+        car_color = gets.chomp
+        new_car = CarDetails.new(car_make, car_model, car_year, car_color)
+        new_car = new_car.to_json
+        bookings = bookings.push(new_car)
+    end
 
 
 while user_input != "quit"
-    if user_input == "add"
-    puts "Enter your make: "
-    car_make = gets.chomp
-    puts "Enter your model: "
-    car_model = gets.chomp
-    puts "Enter car year: "
-    car_year = gets.chomp
-    puts "Enter your car color: "
-    car_color = gets.chomp
-    car1 = CarDetails.new(car_make, car_model, car_year, car_color)
-    File.open("booking.txt", "a") do |file|
-        file.write(car1.to_json)
-    end
-    puts car1.to_json
+    case user_input
+    when "add"
+        puts "Make: "
+        car_make = gets.chomp
+        puts "Model: "
+        car_model = gets.chomp
+        puts "Year: "
+        car_year = gets.chomp
+        puts "Colour: "
+        car_color = gets.chomp
+        new_car = CarDetails.new(car_make, car_model, car_year, car_color)
+        p new_car
+        new_car = new_car.to_json
+        bookings = bookings.push(new_car)
         break
-
-    elsif user_input == "view"
+    when "view"
         puts "Your current booking is: "
-        File.open("booking.txt", "r") do |file|
-            puts file.read()
-    end
-        break
+        bookings.each_with_index do |booking, index|
+            puts "#{index+1}: #{booking}"
+        end
+
+  
+    puts "What would you like to do?"
+    user_input = gets.chomp
 
 
-#     elsif user_input == "delete"
-#         puts "delete a item"
-#         break
-#     else
-#         puts "thank you for using our app"
-#     break
+    when "delete"
+        puts "what index would you like to delete?"
     end
 end
 
-
+puts "thank you for using our app"
--- testing.rb
+++ testing.rb
@@ -1,3 +1,5 @@
+require 'json'
+bookings = []
 class CarDetails
     def initialize (make, model, year, colour)
     @make = make
@@ -5,7 +7,32 @@ class CarDetails
     @year = year
     @colour = colour
     end
-end
+
+   def to_json
+        hash = {}
+        self.instance_variables.each do |var|
+            hash[var] = self.instance_variable_get var
+        end
+        hash.to_json
+    end
+
+    def from_json! string
+        JSON.load(string).each do |var, val|
+            self.instance_variable_set var, val
+        end
+    end
+    end
+# def display_car_make()
+#        puts "Your car is " + CarDetails.make  
+# end
 
 car1 = CarDetails.new("Toyota", "Corolla", "2011", "White")
-p car1
+car2 = CarDetails.new("Nissan", "Micra", "2012", "Yellow")
+
+
+new_car1 = car1.to_json
+bookings = bookings.push(new_car1)
+new_car2 = car2.to_json
+bookings = bookings.push(new_car2)
+
+puts bookings[1]
