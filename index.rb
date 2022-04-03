8362c9e336a38f41cd0b13eef832dad51a7e094d
jimmyray92 <jameswang675203@gmail.com>
2022-04-03 16:41:25 +1000

    added information in about and help file

--- index.rb
+++ index.rb
@@ -1,15 +1,47 @@
 begin
-    require 'json'
-    require 'tty-prompt'
-    require 'rubocop'
-    require 'rspec'
-    require 'colorize'
+  require 'json'
+  require 'tty-prompt'
+  require 'rubocop'
+  require 'rspec'
+  require 'colorize'
 rescue LoadError
     puts 'There has been an error loading the necessary dependencies'
     puts "Please:
         1. navigate to the root directory of this application
         2. run the program again."
     exit!
+end
+
+class CarDetails
+    attr_accessor :make, :model, :pages
+    def initialize (make, model, colour)
+    @list = []
+    @make = make
+    @model = model
+    @colour = colour
+    end
+
+    def to_json
+        hash = {}
+        self.instance_variables.each do |var|
+            hash[var] = self.instance_variable_get var
+        end
+        hash.to_json
+    end
+
+    def from_json! string
+        JSON.load(string).each do |var, val|
+            self.instance_variable_set var, val
+        end
+    end
+end
+
+def file_writing(book)
+    File.write('bookings.json', JSON.pretty_generate(book))
+end
+
+def prints_view_information
+    puts "Your current booking is:"
 end
 
 begin
@@ -35,54 +67,11 @@ begin
         exit
     end
 
-class CarDetails
-    attr_accessor :make, :model, :pages
-    def initialize (make, model, colour)
-    @list = []
-    @make = make
-    @model = model
-    @colour = colour
-    end
-end
-
-    def to_json
-        hash = {}
-        self.instance_variables.each do |var|
-            hash[var] = self.instance_variable_get var
-        end
-        hash.to_json
-    end
-
-    def from_json! string
-        JSON.load(string).each do |var, val|
-            self.instance_variable_set var, val
-        end
-    end
-
-
-
-# def view_bookings
-#     bookings.each_with_index do |booking, index|
-#     puts "#{index+1}: #{booking}"
-# end
-
-
-def file_writing(book)
-    File.write('bookings.json', JSON.pretty_generate(book))
-end
-
-def prints_view_information
-    puts "Your current booking is:"
-end
-
-# def show_current_bookings
-#     puts "you have #{bookings.length} bookings."
-# end
-
+puts ("Welcome to the booking app!").colorize(:green)
 
 loop do
     prompt = TTY::Prompt.new
-    input = prompt.select("What would you like to do?", %w(view add delete exit))
+    input = prompt.select("What would you like to do? Select exit to exit out of the app.", %w(view add delete exit))
     bookings = JSON.load_file('bookings.json', symbolize_names: true)
     
 
@@ -125,9 +114,9 @@ loop do
             puts "Select a index to delete: "
             index = gets.chomp.to_i
             if index <= 0
-                puts "selection cannot be smaller than or equal to 0"
+                puts ("selection cannot be smaller than or equal to 0").colorize(:red)
             elsif index > bookings.length
-                puts "selection does not exist"
+                puts ("selection does not exist").colorize(:red)
             else
             deleted = bookings.slice!(index - 1)
             puts "you have deleted #{deleted}"
--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+bundle install
+
+clear
 
 ruby index.rb
